dbad681a55645ab4f63c35d5f08e3efa7acdcb0e
Keenan Payne <kpayne@slackrmedia.com>
2014-06-25 17:22:35 -0600

    Convert tabs to spaces

--- scripts/concise.js
+++ scripts/concise.js
@@ -3,24 +3,24 @@
 //       Notes: 
 //-----------------------------------------------------------------------------
 (function($) {
-	$.fn.responsiveNav = function() { 
-		// Loop through each instance of responsive navigation
-		this.each(function(index) {	
-			if ($(this).hasClass( "nav-responsive-text" )) {
-			
-				// Store result in variable
-				var labelState = true;
-			} else { 
-				var labelState = false;
-			}
-		
-			// Return `naver` plugin instantiation
-			return $(this).naver({
-				maxWidth: "768px",
-				label: labelState
-			});
-		});
-	};
+    $.fn.responsiveNav = function() { 
+        // Loop through each instance of responsive navigation
+        this.each(function(index) {	
+            if ($(this).hasClass( "nav-responsive-text" )) {
+            
+                // Store result in variable
+                var labelState = true;
+            } else { 
+                var labelState = false;
+            }
+        
+            // Return `naver` plugin instantiation
+            return $(this).naver({
+                maxWidth: "768px",
+                label: labelState
+            });
+        });
+    };
 }(jQuery));
 
 //----------------------------- nonResponsive() -------------------------------
@@ -28,43 +28,43 @@
 //       Notes: Only necessary for non-responsive websites.
 //-----------------------------------------------------------------------------
 (function($) {
-	$.fn.nonResponsive = function() { 
-		// Loop through each instance of the `.non-responsive` class
-		this.each(function(index) {	
-			if ($(this).hasClass("non-responsive")) {
-			
-				// Get container width
-				var containerWidth = $(".container").width();
-				
-				// Calculate object width
-				(function($){
-					$.fn.calculateWidth = function() {
-						var objectWidth = $(this).width();
-						
-						return objectWidth;
-					}; 
-				})(jQuery);
-				
-				// Set pixel-based alternatives for grid styles
-				// But first we need to know if our row has class `gutters`
-				if($(this).hasClass("gutters")) {
-					for ( var i = 1; i <= 24 ; i++ ) {            
-						// Column width for row with gutters
-						var columnWidth = ($('.column-'+i).calculateWidth());
-						var gutterWidth = columnWidth * 0.02;
-						
-						$('.gutters .column-'+i).css("width", columnWidth - gutterWidth + "px");
-					}
-				} else {
-					for ( var i = 1; i <= 24 ; i++ ) {            
-						// Column width for normal row
-						var columnWidth = $('.column-'+i).calculateWidth();
-						$('.column-'+i).css("width", columnWidth - 1 + "px");
-					}
-				}
-			}
-		});
-	};
+    $.fn.nonResponsive = function() { 
+        // Loop through each instance of the `.non-responsive` class
+        this.each(function(index) {	
+            if ($(this).hasClass("non-responsive")) {
+            
+                // Get container width
+                var containerWidth = $(".container").width();
+                
+                // Calculate object width
+                (function($){
+                    $.fn.calculateWidth = function() {
+                        var objectWidth = $(this).width();
+                        
+                        return objectWidth;
+                    }; 
+                })(jQuery);
+                
+                // Set pixel-based alternatives for grid styles
+                // But first we need to know if our row has class `gutters`
+                if($(this).hasClass("gutters")) {
+                    for ( var i = 1; i <= 24 ; i++ ) {            
+                        // Column width for row with gutters
+                        var columnWidth = ($('.column-'+i).calculateWidth());
+                        var gutterWidth = columnWidth * 0.02;
+                        
+                        $('.gutters .column-'+i).css("width", columnWidth - gutterWidth + "px");
+                    }
+                } else {
+                    for ( var i = 1; i <= 24 ; i++ ) {            
+                        // Column width for normal row
+                        var columnWidth = $('.column-'+i).calculateWidth();
+                        $('.column-'+i).css("width", columnWidth - 1 + "px");
+                    }
+                }
+            }
+        });
+    };
 }(jQuery));
 
 //------------------------------- DropDown(el) --------------------------------
@@ -72,30 +72,30 @@
 //       Notes: 
 //-----------------------------------------------------------------------------
 function DropDown(el) {
-	this.dd = el;
-	this.initEvents();
+    this.dd = el;
+    this.initEvents();
 }
 
 DropDown.prototype = {
-	initEvents : function() {
-		var obj = this;
-	
-		// Toggle .dropdown-active on click
-		obj.dd.on('click', function(event){
-			$(this).toggleClass('dropdown-active');
-			event.stopPropagation();
-		});
-	}
+    initEvents : function() {
+        var obj = this;
+    
+        // Toggle .dropdown-active on click
+        obj.dd.on('click', function(event){
+            $(this).toggleClass('dropdown-active');
+            event.stopPropagation();
+        });
+    }
 }
 
 $(function() {
-	var dd = new DropDown( $('.dropdown') );
+    var dd = new DropDown( $('.dropdown') );
 
-	$(document).click(function() {
-	
-		// Remove class from all dropdowns
-		$('.dropdown').removeClass('dropdown-active');
-	});
+    $(document).click(function() {
+    
+        // Remove class from all dropdowns
+        $('.dropdown').removeClass('dropdown-active');
+    });
 });
 
 //-----------------------------------------------------------------------------
@@ -115,14 +115,14 @@ $(function() {
 //       Notes: 
 //-----------------------------------------------------------------------------
 $(function() {
-	$(".progress > span").each(function() {
-		$(this)
-			.data("origWidth", $(this).width())
-			.width(0)
-			.animate({
-				width: $(this).data("origWidth")
-			}, 1200);
-	});
+    $(".progress > span").each(function() {
+        $(this)
+            .data("origWidth", $(this).width())
+            .width(0)
+            .animate({
+                width: $(this).data("origWidth")
+            }, 1200);
+    });
 });
 
 //-------------------------- $(document).ready() ------------------------------
@@ -130,6 +130,6 @@ $(function() {
 //       Notes: 
 //-----------------------------------------------------------------------------
 jQuery(document).ready(function() {	
-	jQuery(".nav-responsive, .nav-responsive-left, .nav-responsive-center").responsiveNav();
-	jQuery("body, .row").nonResponsive();
+    jQuery(".nav-responsive, .nav-responsive-left, .nav-responsive-center").responsiveNav();
+    jQuery("body, .row").nonResponsive();
 });
