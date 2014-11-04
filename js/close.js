5c3feb495a1cf7f7ed67672188c73cedfe222011
Keenan Payne <contact@keenanpayne.com>
2014-11-03 23:00:04 -0800

    Delete files

--- js/close.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-  $(document).on('click', '.close', function () {
-    // Get .close parent div, fade out
-    $(this).parents('.close-this').fadeOut('slow');
-  });
-});
