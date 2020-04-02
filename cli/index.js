09601c970bdada6532acc04a5cbca803ff5914a8
Jaime Hernandez <jaiheravi@gmail.com>
2020-04-02 13:30:27 -0700

    Remove unnecessary cli package

--- cli/index.js
+++ cli/index.js
@@ -1,31 +1,23 @@
 #!/usr/bin/env node
 
-const cliparse = require('cliparse');
-const pkg = require('../package.json');
-const fs = require('fs');
+const fs = require('fs')
+const compile = require('./commands/compile')
 
-const compile = require('./commands/compile');
+const command = {
+  name: process.argv[2],
+  input: process.argv[3],
+  output: process.argv[4]
+}
 
-cliparse.parse(cliparse.cli({
-  name: 'concise-cli',
-  description: 'Command-line Interface for Concise CSS',
-  version: pkg.version,
-  commands: [
-    cliparse.command(
-      'compile', {
-        description: 'Compile code from Concise CSS',
-        args: [
-          cliparse.argument('input', { description: 'File to compile' }),
-          cliparse.argument('output', { description: 'Output CSS file' })
-        ]
-      }, (params) => {
-        compile(params.args[0]).then(css => {
-          fs.writeFile(params.args[1], css, (err) => {
-            if (err) { throw err; }
-            console.log(`File written: ${params.args[1]}\nFrom: ${params.args[0]}`);
-          })
-        })
-      }
-    )
-  ]
-}));
+switch (command.name) {
+  case 'compile':
+    compile(command.input).then(css => {
+      fs.writeFile(command.output, css, err => {
+        if (err) throw err
+        console.log(`File written: ${command.output}\nFrom: ${command.input}`);
+      })
+    })
+    break
+  default:
+    break
+}
