842eddeaddd1acab3ab91a34ea8d93c1e6818035
Jaime Hernandez <jaiheravi@gmail.com>
2020-04-02 13:40:54 -0700

    Remove unnecessary compile file

--- cli/index.js
+++ cli/index.js
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const compile = require('./commands/compile')
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const nested = require('postcss-nested');
+const ifMedia = require('postcss-if-media');
+const scssSyntax = require('postcss-scss');
+const stripComments = require('postcss-strip-inline-comments');
+const mediaMinMax = require('postcss-media-minmax');
+const customMedia = require('postcss-custom-media');
+const imports = require('postcss-easy-import');
+
+const lh = require('../lib/lh');
+const typeScale = require('../lib/type-scale');
+
+const compile = async file => {
+  const ccss = fs.readFileSync(file, 'utf8');
+
+  return await postcss()
+    .use(imports())
+    .use(stripComments())
+    .use(ifMedia())
+    .use(nested())
+    .use(customMedia())
+    .use(mediaMinMax())
+    .use(lh())
+    .use(typeScale())
+    .use(autoprefixer())
+    .process(ccss, { parser: scssSyntax, from: file });
+}
 
 const command = {
   name: process.argv[2],
